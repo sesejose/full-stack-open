c453c3bcd4235e1a2e4eebb08fd63d2e27b036d0
José <sesejose@gmail.com>
2025-05-02 21:34:20 +0200

    Exercises 1.3.-1.5.

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,37 +1,4 @@
 const Header = (props) => {
-<<<<<<< HEAD
-  return <h1>{props.course}</h1>;
-};
-const Content = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </div>
-  );
-};
-const Total = (props) => {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>;
-};
-
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
-  console.log("Content", Content);
-=======
   return <h1>{props.course.name}</h1>;
 };
 
@@ -77,28 +44,19 @@ const App = () => {
       },
     ],
   };
->>>>>>> part1
 
   return (
     <div>
       <Header course={course} />
-<<<<<<< HEAD
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-=======
       <Content course={course} />
       <Total course={course} />
->>>>>>> part1
     </div>
   );
 };
 
 export default App;
-<<<<<<< HEAD
-=======
 
 // Exercise 2: App declare the variables and pass them as props to the Content component;
 // Content pass them to the Part component - vary the values from on instance to another;
 // Part just gives the name to two props, defines the place where to put the variables that comes from top;
 // Remember the values comes as prop={value} from the Part component in Content component, Part just defines where to put them!
->>>>>>> part1
