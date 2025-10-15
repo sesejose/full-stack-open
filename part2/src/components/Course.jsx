cd8ae60dff246a3b902a43e1d89509b5136f75b4
José <sesejose@gmail.com>
2025-10-15 15:31:15 +0200

    Course information step 9

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,39 +1,21 @@
-// App
-//   Course
-//     Header
-//     Content
-//       Part
-//       Part
-//       ...
-
-// This component receives a course object as a prop from App.jsx
-// and renders the course name and its parts with exercises.
-// The subcomponents Header, Content, and Part are defined within Course.
-// This structure helps in organizing the code and making it reusable.
-// The Course component is exported for use in App.jsx.
-
-const Course = ({ course }) => {
-  const Header = ({ course }) => <h1>{course.name}</h1>;
-  const Part = ({ part }) => (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-  const Content = ({ course }) => (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-  const Total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+const Course = ({ courses }) => {
+  const Headline = () => courses.map((i) => <h2 key={i.id}>{i.name}</h2>);
+  const Parts = () =>
+    courses.map((i) =>
+      i.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))
+    );
+  const Total = courses.map((course) => course.parts.reduce((sum, part) => sum + part.exercises, 0));
 
   return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
+    <>
+      <Headline></Headline>
+      <Parts></Parts>
       <p>Total of {Total} excercises </p>
-    </div>
+    </>
   );
 };
 
