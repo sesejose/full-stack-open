85c4443f8d7120ade10c8d102b86ba1d865a54e1
José <sesejose@gmail.com>
2026-01-23 15:03:41 +0100

    Refractoring Application in Baby Steps - Fixing PORT 3001

--- part4/blog-list-app/utils/config.js
+++ part4/blog-list-app/utils/config.js
@@ -8,7 +8,7 @@
 
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = { MONGODB_URI, PORT };
